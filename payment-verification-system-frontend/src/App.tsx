20366dbbed31097b05cc14b9c4df2522da7be8c5
collins <chigbo.c.o@gmail.com>
2023-09-30 14:33:44 +0200

    established session connection to frontend

--- payment-verification-system-frontend/src/App.tsx
+++ payment-verification-system-frontend/src/App.tsx
@@ -2,27 +2,40 @@ import { useEffect, useState } from "react";
 
 
 
-
-
 function App() {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [text, setText] = useState<string>("");
+  const [websocket, setWebSocket] = useState<WebSocket>();
+
  
-const [message, setMessage] = useState<string>("");
 
   useEffect(() => {
-    const socket = new WebSocket("ws://localhost:8081/ws", []);
-  socket.onmessage = () => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //@ts-ignore
-    const m = event?.data
-    setTimeout(() => setMessage(m), 3000)
-  }
+    const ws = new WebSocket("ws://localhost:8081/ws")
+    setWebSocket(ws);
+  
+    ws.onmessage = (event) => {
+      setMessages((prev) => [...prev, event.data])
+    }
+
+  
 
   },[])
+
+  function sendMessage(){
+    websocket?.send(text);
+    setText("");
+    console.log(text)
+  }
+
+ 
   return (
     <>
-   Hello
-   <div>{message}</div>
-     
+   <div>Display</div>
+   {messages.map((message , index)=> (
+    <div key={index}>{message}</div>
+   ))}
+   <input type="text" onChange={(e) => setText(e.target.value)}/>
+   <button onClick={sendMessage}>send</button>
     </>
   )
 }
